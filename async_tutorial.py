d7f14237f6a46c46f080b27168ce2eded0d6f403
Gyungmin Myung <ryan000502@gmail.com>
2022-11-09 18:04:33 +0900

    add arduino ino files & intellisense config

--- async_tutorial.py
+++ async_tutorial.py
@@ -1,10 +1,20 @@
-async def do_async():
+import asyncio
+import math
+import mpu6050
+
+async def periodic():
+    while True:
+        print('periodic')
+        await asyncio.sleep(1)
+
+def stop():
+    task.cancel()
+
+loop = asyncio.get_event_loop()
+loop.call_later(5, stop)
+task = loop.create_task(periodic())
+
+try:
+    loop.run_until_complete(task)
+except asyncio.CancelledError:
     pass
-
-async def main_async():
-    
-    await do_async()
-
-a = 1
-
-print(a)
